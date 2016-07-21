54e11679a8842a394c25c2294f6dda8df99b6df6
seanreed1111 <seanreed1111@gmail.com>
2016-07-21 10:28:01 -0400

    adds GroceryList and ListItem components

--- source/App.js
+++ source/App.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-class Hello extends React.Component {
+// Parent Component
+class GroceryList extends Component{
   render() {
     return (
-      <h1>Hello World</h1>
+      <ul>
+        <ListItem quantity="1" name="Bread" />
+        <ListItem quantity="6" name="Eggs" />
+        <ListItem quantity="2" name="Milk" />        
+      </ul>
     );
   }
 }
 
-React.render(<Hello />, document.getElementById('root'));
+//child component
+class ListItem extends Component {
+  render() {
+    return (
+      <li>
+        {this.props.quantity} x {this.props.name}
+      </li>
+    );
+  }
+}
+
+React.render(<GroceryList />, document.getElementById("root"));
+
+
+
+// class Hello extends Component {
+//   render() {
+//     var place = "USA";
+//     return (
+//       <div>
+//       <h1>Hello {place} I'm Here</h1>
+
+//       </div>
+//     );
+//   }
+// }
+
+// React.render(<Hello />, document.getElementById('root'));
